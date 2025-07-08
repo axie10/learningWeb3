03c72992b74bb438faf26a9ed9faf609fac0f366
axie10 <cryptosushi21@gmail.com>
2025-07-08 09:57:38 +0200

    Refactor Food and Hamburguer contracts for improved readability by adjusting formatting and ensuring consistent spacing

--- contracts/2_advance_level_concepts/2_advanced_functions.sol
+++ contracts/2_advance_level_concepts/2_advanced_functions.sol
@@ -4,36 +4,42 @@
 pragma solidity ^0.8.4;
 
 contract Food {
+
     // Struct
     struct dinnerPlate {
         string name;
         string ingredients;
     }
 
-    dinnerPlate[] menu;
+    dinnerPlate [] public menu;
 
     // Creation a internal function to call in heritage contracts
     function newMenu(string memory _name, string memory _ingredients) internal {
         menu.push(dinnerPlate(_name, _ingredients));
     }
+
+    function showMenu() public view returns(dinnerPlate [] memory menu_) {
+        menu_ = menu;
+    }
 }
 
-contract Hamburguer is Food {
+contract Hamburguer is  Food{
+
     address public owner;
 
     // modifier
-    modifier ownerOnly() {
-        require(owner == msg.sender,"you have not permission to run this function");
+    modifier ownerOnly(){
+        require(owner == msg.sender, "you have not permission to run this function");
         _;
     }
 
-    // In the constructor
+    // Constructor to initialize owner variable
     constructor() {
         owner = msg.sender;
     }
 
     // function call to functions that heritage of father contract
-    function doHamburger(string memory ingredients_,uint limithamburguer_) external ownerOnly {
+    function doHamburger(string memory ingredients_, uint limithamburguer_) external ownerOnly {
         require(limithamburguer_ < 5, "max 5 hambuguers");
         newMenu("Hamburguer", ingredients_);
     }
